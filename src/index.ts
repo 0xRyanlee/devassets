692f5f80e2673cf66097976562fc3fcf52811706
0xRyanlee <ryan910814@gmail.com>
2026-06-27 17:59:42 +0800

    fix(cli): handle EPIPE on stdout for piped output (get --raw | head)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import { program } from './cli.js';
 
+process.stdout.on('error', (err) => { if (err.code === 'EPIPE') process.exit(0); });
+
 program.parseAsync(process.argv).catch(err => {
   console.error(err);
   process.exit(1);
